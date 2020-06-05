54984b32330371de6f65a7b8d0eee020f5e4b0f2
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-06-04 21:25:43 -0400

    setting up enviorment for tiktactor game

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDom from' react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+    }
+}
