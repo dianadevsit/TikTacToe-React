8ea5225b7c662975e70dda4b89b8a7437cbc285b
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-06-07 19:10:24 -0400

    updated with some adjustments:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import './index.html';
 
 // The Square function renders a single <button>
 function Square(props) {
