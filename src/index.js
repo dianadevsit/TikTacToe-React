0fb6811248241c0848290428ffdcda0d0b9cc017
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2020-06-07 16:26:30 -0400

    player may now click on squares and game will state whether 'X' or 'O' will be needed to click next. winner will be stated once array function returns as appropriate

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ 
       squares: squares, 
@@ -35,8 +38,15 @@ class Board extends React.Component {
     />;
   }
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
+      //the rest has not changed
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
@@ -82,3 +92,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+// Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
